ca722e63d3131695206d32204bd752f71ddcc45f
Drew <80695992+MoeTheBeast@users.noreply.github.com>
2022-07-11 22:24:01 -0400

    Create Ex3.py

---
+++ Input_Output_Exercises/Ex3.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+# Here we get an int input from the user
+num = int(input("Pick a number: "))
+
+# Here we just made a vribaile that spits out what the octal number od the decimal number of the picked number is 
+final_words = 'The octal number of decimal number: '
+
+# Here we're is printing what needs to be done
+print(final_words, '%o' % num)
