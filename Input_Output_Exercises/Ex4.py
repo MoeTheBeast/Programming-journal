46bf1429b25bc7912ba2873da9ba47c03d3f3d4c
Drew <80695992+MoeTheBeast@users.noreply.github.com>
2022-07-11 22:33:07 -0400

    Create Ex4.py

---
+++ Input_Output_Exercises/Ex4.py
@@ -0,0 +1,9 @@
+
+
+
+
+# Here we get a decimal input from the user
+num = float(input("Pick a number that also has a decimal: "))
+
+# Here we print and use the opprtertion to print out the number that the user picked and up to 2 decimal places 
+print('%.2f' % num)
